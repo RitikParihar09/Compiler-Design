f0c939e91ce284394855cbd096622c2a9bee95f4
Ritik Parihar <146638282+RitikParihar09@users.noreply.github.com>
2025-03-18 11:40:15 +0530

    Add files via upload

---
+++ in.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* This is a
+   multi-line comment */
+
+int main() {
+    // This is a single-line comment
+    int a = 10;  // initialize a with 10
+    int b = 20;  /* initialize b with 20 */
+    
+    printf("Sum: %d\n", a + b); // printing the sum
+
+    return 0; // end of main
+}
